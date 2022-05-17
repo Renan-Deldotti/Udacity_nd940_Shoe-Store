2be8901882f519217a565ad2f9a8db5fdde31dca
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-05-16 22:38:49 -0300

    Add login fragment

--- app/src/main/java/com/test/shoestore/MainActivity.kt
+++ app/src/main/java/com/test/shoestore/MainActivity.kt
@@ -2,10 +2,15 @@ package com.test.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.test.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/test/shoestore/login/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.test.shoestore.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.test.shoestore.R
+
+class LoginFragment : Fragment() {
+    companion object {}
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
