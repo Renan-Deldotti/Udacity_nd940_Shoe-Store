3a65e11fba9950398b038fad88390e365e4db883
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-05-17 00:02:48 -0300

    Add WelcomeFragment

--- app/src/main/java/com/test/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/test/shoestore/login/LoginFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.test.shoestore.R
+import com.test.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    companion object {}
+
+    private lateinit var loginFragmentBinding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        loginFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+
+        loginFragmentBinding.loginButton.setOnClickListener { v ->
+            v.findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(loginFragmentBinding.editTextUserName.text.toString())
+            )
+        }
+
+        loginFragmentBinding.createAccountButton.setOnClickListener { v ->
+            v.findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(loginFragmentBinding.editTextUserName.text.toString())
+            )
+        }
+
+        return loginFragmentBinding.root
     }
 
 }
---
+++ app/src/main/java/com/test/shoestore/welcome/WelcomeFragment.kt
@@ -0,0 +1,31 @@
+package com.test.shoestore.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.test.shoestore.R
+import com.test.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var args: WelcomeFragmentArgs
+    private lateinit var welcomeFragmentBinding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        args = WelcomeFragmentArgs.fromBundle(requireArguments())
+        welcomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        // Add a default "Shoer" username if the field is empty
+        val userName = if (args.username.isNullOrEmpty()) "Shoer" else args.username
+
+        welcomeFragmentBinding.welcomeMainTV.text = getString(R.string.welcome_with_username, userName)
+
+        return welcomeFragmentBinding.root
+    }
+}
